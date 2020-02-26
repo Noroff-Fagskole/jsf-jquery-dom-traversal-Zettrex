dd673ff85459fcb9ce9ac6b4acba3035be4e5699
zettrex <zettrex@hotmail.com>
2020-02-26 14:35:02 +0100

    done assignment

---
+++ js/master.js
@@ -0,0 +1,12 @@
+$('#question1').next().addClass('answer');
+$('.question2').prev().addClass('answer');
+$('.question3').children('span').addClass('answer');
+$('.question4').children().addClass('answer');
+$('.question5').find('span').addClass('answer');
+$('.question6').find('p').addClass('answer');
+$('.question7').prev('div').children('p').addClass('answer');
+$('.question8').siblings().addClass('answer');
+$('.question9').prev().addClass('answer');
+$('.question10').siblings().last().addClass('answer');
+$('.question11').closest('div').addClass('answer');
+$('.question12').parent().next().addClass('answer');
